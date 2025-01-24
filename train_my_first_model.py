ca251f3221e7df1f0fbc81546006dc3c296c0060
cleonguyen <cleo.nguyen@gmail.com>
2025-01-24 14:18:00 +1100

    Initial version of training model

---
+++ train_my_first_model.py
@@ -0,0 +1,85 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments
+from datasets import load_dataset
+
+# Load the base model and tokenizer
+model_id = "meta-llama/Llama-3.2-1B-Instruct"
+model = AutoModelForCausalLM.from_pretrained(
+    # Must be float32 for MacBooks!
+    model_id, torch_dtype=torch.float32, device_map="auto")
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token = tokenizer.eos_token
+
+# Load the training dataset
+dataset = load_dataset(
+    "csv", data_files="data/whoami.csv", split="train")
+
+# Define a function to apply the chat template
+
+
+def apply_chat_template(example):
+    messages = [
+        {"role": "user", "content": example['question']},
+        {"role": "assistant", "content": example['answer']}
+    ]
+    prompt = tokenizer.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    return {"prompt": prompt}
+
+
+# Apply the chat templatefunction to the dataset
+new_dataset = dataset.map(apply_chat_template)
+new_dataset = new_dataset.train_test_split(
+    0.05)  # Let's keep 5% of the data for testing
+
+# Tokenize the data
+
+
+def tokenize_function(example):
+    tokens = tokenizer(
+        example['prompt'], padding="max_length", truncation=True, max_length=128)
+    # Set padding token labels to -100 to ignore them in loss calculation
+    tokens['labels'] = [
+        -100 if token == tokenizer.pad_token_id else token for token in tokens['input_ids']
+    ]
+    return tokens
+
+
+# Apply tokenize_function to each row
+tokenized_dataset = new_dataset.map(tokenize_function)
+tokenized_dataset = tokenized_dataset.remove_columns(
+    ['question', 'answer', 'prompt'])
+
+# Define training arguments
+model.train()
+training_args = TrainingArguments(
+    output_dir="./output_my_first_model",
+    eval_strategy="steps",  # to evaluate during training
+    eval_steps=40,
+    logging_steps=40,
+    save_steps=150,
+    per_device_train_batch_size=2,  # Adjust based on your hardware
+    per_device_eval_batch_size=2,
+    num_train_epochs=2,  # How many times to loop through the dataset
+    fp16=False,  # Must be False for MacBooks
+    report_to="none",  # Here we can use something like tensorboard to see the training metrics
+    log_level="info",
+    learning_rate=1e-5,  # Would avoid larger values here
+    max_grad_norm=2  # Clipping the gradients is always a good idea
+)
+
+# Initialize Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["test"],
+    tokenizer=tokenizer)
+
+# Train the model
+trainer.train()
+
+# Save the model and tokenizer
+trainer.save_model("./output_my_first_model")
+tokenizer.save_pretrained("./output_my_first_model")
